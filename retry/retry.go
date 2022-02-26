40d45bc24b12b1c16120e4f896f3068965b485fb
chenzhihui <zhihui_chen@foxmail.com>
2022-02-26 13:40:35 +0800

    add backkof retry

---
+++ retry/retry.go
@@ -0,0 +1,80 @@
+package retry
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-kratos/exp/backoff"
+)
+
+// Option is retry option.
+type Option func(*Retry)
+
+// WithAttempts with attempts.
+func WithAttempts(n int) Option {
+	return func(o *Retry) {
+		o.attempts = n
+	}
+}
+
+// WithRetryable with retryable.
+func WithRetryable(r Retryable) Option {
+	return func(o *Retry) {
+		o.retryable = r
+	}
+}
+
+// WithBackoff with backoff.
+func WithBackoff(b backoff.Strategy) Option {
+	return func(o *Retry) {
+		o.backoff = b
+	}
+}
+
+// Retryable is used to judge whether an error is retryable or not.
+type Retryable func(err error) bool
+
+// Retry config.
+type Retry struct {
+	backoff   backoff.Strategy
+	retryable Retryable
+	attempts  int
+}
+
+// New new a retry with backoff.
+func New(opts ...Option) *Retry {
+	r := &Retry{
+		attempts:  3,
+		retryable: func(err error) bool { return true },
+		backoff:   backoff.New(),
+	}
+	for _, o := range opts {
+		o(r)
+	}
+	return r
+}
+
+// Do wraps func with a backoff to retry.
+func (r *Retry) Do(ctx context.Context, fn func(context.Context) error) error {
+	var (
+		err     error
+		retries int
+	)
+	for {
+		if err = ctx.Err(); err != nil {
+			break
+		}
+		if err = fn(ctx); err == nil {
+			break
+		}
+		if err != nil && !r.retryable(err) {
+			break
+		}
+		retries++
+		if r.attempts > 0 && retries >= r.attempts {
+			break
+		}
+		time.Sleep(r.backoff.Backoff(retries))
+	}
+	return err
+}
---
+++ retry/retry_test.go
@@ -0,0 +1,49 @@
+package retry
+
+import (
+	"context"
+	"errors"
+	"testing"
+)
+
+func TestRetry(t *testing.T) {
+	r := New()
+	{
+		if err := r.Do(context.Background(), func(ctx context.Context) error {
+			t.Log("retry ok")
+			return nil
+		}); err != nil {
+			t.Fatal(err)
+		}
+	}
+	{
+		n := 0
+		err := r.Do(context.Background(), func(ctx context.Context) error {
+			n++
+			t.Log("retry:", n)
+			return errors.New("retry")
+		})
+		if err == nil {
+			t.Log("should return an error")
+		}
+		if n != r.attempts {
+			t.Logf("should want %d but got %d", r.attempts, n)
+		}
+	}
+	{
+		nr := New(WithRetryable(func(err error) bool {
+			return false
+		}))
+		n := 0
+		err := nr.Do(context.Background(), func(ctx context.Context) error {
+			n++
+			return errors.New("not retry")
+		})
+		if err == nil {
+			t.Log("should got an error")
+		}
+		if n != 1 {
+			t.Log("should not retry")
+		}
+	}
+}
